5c96c4c13007c99818d576be49844422932fb3f8
Simeon Bird <spb@ias.edu>
2013-07-18 18:13:39 +0200

    Replace the fancy UVB attenuation (which did nothing) with just attenuating the parts of the UVB between the Lyman limits

--- make_cloudy_input.py
+++ make_cloudy_input.py
@@ -32,109 +32,27 @@ class UVBAtten(cold_gas.RahmatiRT):
         #Doing this right is really too complicated.
         return self.photo_rate(10**hden, 10**temp)/self.gamma_UVB
 
-def broad(lambda_X, gamma_X):
-    """Natural broadening parameter, a, for a line.
-    This is computed by taking the Voigt profile in the limit that doppler broadening is zero,
-    then multiplying by λ_D/λ_X, where λ_D is the Doppler broadening parameter.
-    Thus we have a' = λ_X Γ_X/ 4π c
-    Arguments:
-    lambda_X - transitions wavelength in m
-    gamma_X - transition probability in Hz
-    """
-    return (lambda_X * gamma_X)/(4*math.pi*2.9979e8)
-
-def sigma_X(lambda_X, fosc_X):
-    """The cross-section of the absorption transition, in m^2.
-       This is the sqrt of the Thompson cross-section for the photon times the x-section of the transition.
-    """
-    sigma_T = 6.652458558e-29 #Thompson cross-section in m^2
-    return np.sqrt(3.0*math.pi/8.0*sigma_T)*lambda_X*fosc_X
-
-def profile(ll, lambda_X, gamma_X, fosc_X, hden):
-    """The profile for the absorption we assume to be Lorentzian, in the limit where there is no
-    thermal broadening of the line.
-
-    To compute it, we use F(l) = F_0 (1 - exp(-τ)), where F_0 is chosen so that the profile
-    is unity at the transition, its maximal point.
-
-    The optical depth, τ is given by
-    τ = (σ_X c N) / (sqrt{π} b) H(a,x)
-    where H(a,x) is the Voigt profile.
-    In the limit where T-> 0, the thermal parameter, b-> 0, and the Voigt profile becomes:
-
-    H(a, x) = (b/c) α / (π ( ζ^2 + α^2))
-
-    where α = λ_i Γ_i / (4 π c) and ζ = (λ - λ_i) / λ_i
-
-    Then we have
-
-    τ = σ_X N α / (π^(3/2) ( ζ^2 + α^2))
-
-    """
-    aa = broad(lambda_X, gamma_X)
-    xx = (ll - lambda_X)/lambda_X
-    ss = sigma_X(lambda_X, fosc_X)
-    #Column density: characteristic length of each particle is assumed to be 1 kpc/h.
-    #Note hden is given in log([1/cm^3])
-    cdd = 10**hden*1e6*3.1e19/0.7
-
-    tau = ss*cdd*aa/(xx**2+aa**2)/math.pi**1.5
-
-    f0 = 1-np.exp(-ss*cdd/aa/math.pi**1.5)
-    flux = (1-np.exp(-tau))/f0
-    return flux
-
 def gen_cloudy_uvb_shape_atten(uvb_table, redshift, hden,temp):
     """
-    Generate the cloudy input string from a UVB table, with some more physical attenuation.
-    Assume that the gas is optically thick only to HI and HeI, as all other species are a
-    factor of 10^4 less abundant. Neglect H2 absorption.
-
-    The attenuation is that calculated by Rahmati 2012. This was only calculated for H Lya, so for
-    other lines we scale the attenuation with the optical depth. The optical depth goes like (for fixed columns):
-    τ ~ σ_X n
-    so for other transitions model the change in cross-section by adjusting the density to get the same τ.
-    So compute attenuation at new density n' = n (σ_X'/σ_X) = n /(λ_X' / λ_X) (f^osc_X'/f^osc_X)
-    This is not really right - it neglects the fact that the transition rates are also getting smaller,
-    which will make for less self-shielding from these lines.
-
-    Ly-beta is about a factor of 5 smaller in x-section, ly-gamma another.
-    Higher order transition rates are also smaller, making the line less broad, and
-    fewer relevant metal transitions are at higher energies.
-
-    Then the effect is moved to densities rather near the star-forming regime, where we are less confident,
-    and our quick approximation becomes increasingly bad.
-    Because of CIE at these densities all we really want is the right number of free electrons, which we
-    get by having the HI and HeI fractions right.
-
-    So we just cut the UVB from Lya, lyb and He Lya, and assume everything else does not
-    significantly attenuate the UVB.
-
-    The profile around each shielded transition is Lorentzian, for a DLA in the naturally broadened limit.
-
+    Generate the cloudy input string from a UVB table, with slightly more physical attenuation.
+    Attenuate uniformly (following the Rahmati prescription) only the regions
+    between Lya and the Lyman limit for H and He. For He, divide the density by 3, as He is 1/3 as abundant.
     Note Rydberg ~ 1/wavelength, and 1 Rydberg is the energy of a photon at the Lyman limit, ie,
-    with wavelength 91.18 nm.
+    with wavelength 911.8 Angstrom.
     """
     UVB = UVBAtten(redshift)
-    #Subsample the UVB so we have more points around the regions we care about
-    intuvb = interp1d(uvb_table[:,0], uvb_table[:,1])
-    newuvb = np.append(uvb_table[:,0], 91.18e-9/(1100+np.arange(0,200,10)))
-    newuvb = np.append(newuvb, 91.18e-9/(520+np.arange(0,100,10)))
-    newuvb = np.sort(newuvb)
-    origuvb = 10**intuvb(newuvb)
-    waveuvb = 91.18e-9/newuvb
-    #Calculate the profile of absorption from HI Lya data from VPFIT.
-    lya_prof = origuvb*(1-UVB.atten(hden, temp))*profile(waveuvb, 1215.6701*1e-10, 6.265e8, 0.4164, hden)
-    #Do Ly-beta: cross-section
-    #sbeta = (1025.7223*0.07912)/(1215.6701*0.4164)
-    #lyb_prof = origuvb*(1-UVB.atten(hden*sbeta, temp))*profile(waveuvb, 1025.7223*1e-10, 1.897e8, 0.079129, hden)
-    #Now calculate the profile of absorption from He Lya: data again from VPFIT.
-    saHe = (584.334*0.285)/(1215.6701*0.4164)
-    lyaHe_prof = origuvb*(1-UVB.atten(hden*saHe, temp))*profile(waveuvb, 584.334*1e-10, 2e8, 0.285, hden)
+    waveuvb = 911.8/uvb_table[:,0]
+    #Attenuate uniformly the part of the UVB between Lya and the Lyman limit for H
+    profile = np.zeros_like(uvb_table[:,0])
+    ind = np.where((waveuvb < 1220)*(waveuvb > 900))
+    profile[ind]=1.
     #Compute adjusted UVB table
-    subuvb = origuvb - (lya_prof+lyaHe_prof)
-    subuvb[np.where(subuvb < 1.e-35)] = 1.e-35
-    uvb_table[:,1] = np.log10(subuvb)
+    uvb_table[:,1] -= np.log10(UVB.atten(hden, temp))*profile
+    #Attenuation for He
+    profile = np.zeros_like(uvb_table[:,0])
+    ind = np.where((waveuvb < 590)*(waveuvb > 510))
+    profile[ind]=1.
+    uvb_table[:,1] -= np.log10(UVB.atten(hden-np.log10(3), temp))*profile
     #First output very small background at low energies
     uvb_str = "interpolate ( 0.00000001001 , -35.0)\n"
     uvb_str+="continue ("+str(uvb_table[0,0]*0.99999)+", -35.0)\n"
